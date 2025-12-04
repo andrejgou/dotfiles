d5163012c14e81c2e5370ac9012d363274ee027e
Andre Gou <andrejgou@gmail.com>
2025-12-03 21:14:29 -0600

    Remove netrw keymap, add git aliases (sw, swc, rbc, rs)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -161,7 +161,6 @@ require("lazy").setup({
 })
 
 -- Additional keymaps
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
